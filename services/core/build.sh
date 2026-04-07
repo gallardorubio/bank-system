e23274f07132babf3b63294eee1bd0605621088e
alvarogall <varosg@proton.me>
2026-04-07 20:31:48 +0200

    refactor: rename record component to core macro-service and restructure packages

--- services/core/build.sh
+++ services/core/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 mvn clean package -DskipTests
-docker build -t record:latest .
+docker build -t core:latest .
--- services/core/src/main/java/io/github/gallardorubio/banksystem/core/record/dto/OperationCompleted.java
+++ services/core/src/main/java/io/github/gallardorubio/banksystem/core/record/dto/OperationCompleted.java
@@ -1,9 +1,13 @@
 package io.github.gallardorubio.banksystem.record.dto;
 
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+
 public record OperationCompleted(
 
     @NotNull 
-    UUID operationId,
+    UUID operationId
 
 ) 
 {}
