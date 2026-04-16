024c2d730231fab289cfc29dfd2c370ab7c1076e
alvarogall <varosg@proton.me>
2026-04-16 19:51:21 +0200

    chore(users): initialize users microservice skeleton

--- services/core/src/main/java/io/github/gallardorubio/banksystem/core/loan/dto/LoanDetails.java
+++ services/core/src/main/java/io/github/gallardorubio/banksystem/core/loan/dto/LoanDetails.java
@@ -1,8 +1,12 @@
 package io.github.gallardorubio.banksystem.core.loan.dto;
 
 import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public record LoanDetails(
-    @NotNull UUID targetAccountId
+    @NotNull UUID ownerId,
+    @NotNull UUID targetAccountId,
+    @NotNull Integer termPeriods,
+    @NotNull BigDecimal interestRate
 ) {}
